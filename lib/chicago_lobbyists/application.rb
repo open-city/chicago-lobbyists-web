2393a43e212458f698075f6a6d7dbfd088506042
Ryan Briones <ryan.briones@brionesandco.com>
2011-07-16 12:29:31 -0500

    nav menu current-ness

--- lib/chicago_lobbyists/application.rb
+++ lib/chicago_lobbyists/application.rb
@@ -4,9 +4,18 @@ module ChicagoLobbyists
       def cl_erb(template)
         erb template.to_sym
       end
+      
+      def current_menu
+        @current_menu
+      end
+      
+      def current_menu_class(menu_name)
+        return "current" if current_menu == menu_name
+      end
     end
     
     get "/" do
+      @current_menu = "home"
       erb :landing
     end
     
@@ -16,6 +25,16 @@ module ChicagoLobbyists
     
     get "/firms/:id" do
       erb :firm
+    end
+    
+    get "/:page" do |page_name|
+      template = File.join(settings.views, page_name + ".erb")
+      if File.exists?(template)
+        @current_menu = page_name
+        erb page_name.to_sym
+      else
+        pass
+      end
     end
   end
 end
