c2173088fb5bfff972ea1950aa81ed5ea9391311
Derek Eder <derek.eder@gmail.com>
2011-07-19 20:00:47 -0500

    removed slug and client name from firm relationships table

--- db/2010-load_chi_lobbyist_firm_relationships.sql
+++ db/2010-load_chi_lobbyist_firm_relationships.sql
@@ -1,12 +1,10 @@
 -- CHICAGO LOBBYIST CLIENTS
 DROP TABLE public.chi_lobbyist_firm_relationships;
 CREATE TABLE public.chi_lobbyist_firm_relationships  ( 
-	id           	serial NOT NULL,
-  slug          varchar(150) NOT NULL,
+  id           	serial NOT NULL,
   lobbyist_id   integer NOT NULL,
   firm_id       integer NOT NULL,
   client_id     integer NOT NULL,
-  client_name   varchar(150) NULL,
 
   CONSTRAINT chi_lobbyist_firm_relationships_pk PRIMARY KEY(id),
   CONSTRAINT chi_lobbyist_firm_relationships_unique UNIQUE(lobbyist_id, firm_id, client_id)
@@ -15,9 +13,8 @@ CREATE TABLE public.chi_lobbyist_firm_relationships  (
 
 -- CHICAGO LOBBYIST CLIENTS
 INSERT INTO public.chi_lobbyist_firm_relationships(
-  slug, lobbyist_id, firm_id, client_id, client_name)
-SELECT DISTINCT lower(replace(replace(replace(replace(replace(replace(replace(replace(replace(client.client_name, ',', '-'), '.', ''), ' ', '-'), '''', ''), '/', ''), '&', 'and'), '--', '-'), ')', ''), '(', '-') || '-' || cast(l.id as varchar) || '-' || cast(firm.id as varchar)),
-  l.id, firm.id, client.id, client.client_name
+  lobbyist_id, firm_id, client_id)
+SELECT DISTINCT l.id, firm.id, client.id
 FROM public.chi_lobbyists l
   INNER JOIN public.lobbyists client
   ON l.first_name = client.first_name and l.last_name = client.last_name
