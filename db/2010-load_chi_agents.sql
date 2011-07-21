16c5a47971bc660dd24dcd923885f621b657c5f8
Chad Pry <chad.pry@gmail.com>
2011-07-20 21:22:19 -0500

    + corrected comment

--- db/2010-load_chi_agents.sql
+++ db/2010-load_chi_agents.sql
@@ -1,4 +1,4 @@
--- CHICAGO LOBBYIST FIRMS
+-- CHICAGO LOBBYIST AGENTS
 DROP TABLE public.chi_agents;
 CREATE TABLE public.chi_agents  (
   id            serial NOT NULL,
@@ -12,7 +12,7 @@ CREATE TABLE public.chi_agents  (
 );
 
 
--- LOAD CHICAGO LOBBYIST FIRMS
+-- LOAD CHICAGO LOBBYIST AGENTS
 INSERT INTO public.chi_agents (slug, name)
 SELECT DISTINCT lower(replace(replace(replace(replace(agency_code, ',', ''), '.', ''), ' ', '-'), '&', 'and')), agency_code
 FROM public.lobbyist_agencies;
