899b8c12a3c8265ee6414cd9a16f3013d0a5954f
Derek Eder <derek.eder@gmail.com>
2011-11-27 13:02:42 -0600

    + added addresses to agencies

--- db/2010-round2/load.sh
+++ db/2010-round2/load.sh
@@ -1,23 +1,25 @@
 # INITIAL FILE LOADS
 
-psql --dbname=$1 -f setup-agencies.sql
-psql --dbname=$1 -f setup-lobbyist-agencies.sql
-psql --dbname=$1 -f setup-lobbyist-categorized-expenditures.sql
-psql --dbname=$1 -f setup-lobbyist-compensation.sql
-psql --dbname=$1 -f setup-lobbyist-gifts.sql
-psql --dbname=$1 -f setup-lobbyist-major-expenditures.sql
-psql --dbname=$1 -f setup-lobbyist-registry.sql
-psql --dbname=$1 -f setup-lobbyist-terminations.sql
+#psql --dbname=$1 -f setup-agencies.sql
+#psql --dbname=$1 -f setup-lobbyist-agencies.sql
+#psql --dbname=$1 -f setup-lobbyist-categorized-expenditures.sql
+#psql --dbname=$1 -f setup-lobbyist-compensation.sql
+#psql --dbname=$1 -f setup-lobbyist-gifts.sql
+#psql --dbname=$1 -f setup-lobbyist-major-expenditures.sql
+#psql --dbname=$1 -f setup-lobbyist-registry.sql
+#psql --dbname=$1 -f setup-lobbyist-terminations.sql
+#psql --dbname=$1 -f setup-lobbyist_city_payments.sql
 
 
 # CREATE PRODUCTION TABLES
 
-#psql --dbname=$1 -f load_chi_lobbyists.sql
-#psql --dbname=$1 -f load_chi_addresses.sql
-#psql --dbname=$1 -f load_chi_firms.sql
-#psql --dbname=$1 -f load_chi_firm_addresses.sql
-#psql --dbname=$1 -f load_chi_clients.sql
-#psql --dbname=$1 -f load_chi_agencies.sql
+psql --dbname=$1 -f load_chi_addresses.sql
+psql --dbname=$1 -f load_chi_lobbyists.sql
+psql --dbname=$1 -f load_chi_agencies.sql
+psql --dbname=$1 -f load_chi_firms.sql
+psql --dbname=$1 -f load_chi_firm_addresses.sql
+psql --dbname=$1 -f load_chi_clients.sql
+
 #psql --dbname=$1 -f load_chi_actions.sql
 #psql --dbname=$1 -f load_chi_lobbyist_firm_relationships.sql
 #psql --dbname=$1 -f load_chi_client_firm_lobbyist_relationships.sql
--- db/2010-round2/load_chi_agencies.sql
+++ db/2010-round2/load_chi_agencies.sql
@@ -3,8 +3,8 @@ DROP TABLE public.chi_agencies;
 CREATE TABLE public.chi_agencies  (
   id            serial NOT NULL,
   slug          varchar(150) NOT NULL,
-  code          varchar(10) NOT NULL,
   name          varchar(150) NOT NULL,
+  description	text NULL,
   address_id    integer NULL,
 
   CONSTRAINT chi_agencies_pk PRIMARY KEY(id),
@@ -12,10 +12,13 @@ CREATE TABLE public.chi_agencies  (
 );
 
 
--- LOAD CHICAGO LOBBYIST AGENTS
-INSERT INTO public.chi_agencies (slug, code, name)
-SELECT DISTINCT lower(replace(replace(replace(replace(replace(ac.name, ',', ''), '.', ''), ' ', '-'), '&', 'and'), '''', '')), a.agency_code, ac.name
+-- LOAD CHICAGO LOBBYIST AGENCIES
+INSERT INTO public.chi_agencies (slug, name, description, address_id)
+SELECT DISTINCT lower(replace(replace(replace(replace(replace(a.name, ',', ''), '.', ''), ' ', '-'), '&', 'and'), '''', '')), a.name, a.description, adr.id
 FROM public.lobbyist_agencies a
-  LEFT OUTER JOIN public.lobbyist_agency_codes ac
-  ON a.agency_code = ac.code;
-
+  INNER JOIN public.chi_addresses adr
+  ON adr.street = a.address
+    AND adr.city = a.city
+    AND adr.state = a.state
+    AND adr.zip = a.zip
+    AND adr.country = a.country
